e7f4501a80046b0aca0171933f2ff0d88c0627b3
duduzinmuller <eduardomuller989@gmail.com>
2025-07-15 17:59:47 -0400

    feat: ensure restaurants can be searched for

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,11 +1,18 @@
+"use client";
+
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 const Header = () => {
+  const router = useRouter();
   return (
     <div className="flex justify-between px-5 pt-6">
-      <div className="relative h-[30px] w-[100px]">
+      <div
+        className="relative h-[30px] w-[100px]"
+        onClick={() => router.back()}
+      >
         <Image src="/logo.png" alt="FSW Foods" fill className="object-cover" />
       </div>
       <Button
